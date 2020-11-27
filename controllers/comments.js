700607e8295fd00daa4351435ee7b25167a37cf1
Antonio Conrado <antoniojconradojunior@hotmail.com>
2020-11-27 02:26:04 +0000

    comments err handler

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,19 +3,33 @@ const comments = require('../models/comments')();
 module.exports = () =>{
 
     const getAll = async(req, res) =>{
-        res.json(await comments.getCommentsForIssue(req.params.issueNumber))
+        const {getComment, error} = await comments.getCommentsForIssue(req.params.issueNumber)
+        if(error){
+            res.status(500).json({
+                error,
+            })
+        }
+        res.json(getComment);
     }
 
     const getComment = async(req, res)=>{
-        res.json(await comments.getOneComment(req.params.commentId))
+        const {comment, error} = await comments.getOneComment(req.params.commentId)
+        if(error){
+            res.status(500).json({
+                error,
+            })
+        }
+        res.json(comment)
     }
 
     const postComment = async(req, res) => {
-       let issueNumber = req.params.issueNumber;
-       let text = req.body.text;
-       let author = req.body.author;
-
-        const result = await comments.addComment(issueNumber, text, author);
+        const {issueNumber, text, author} = req.body;
+        const {result, error} = await comments.addComment(issueNumber, text, author);
+        if(error){
+            res.status(500).json({
+                error,
+            })
+        }
         res.json(result);
     }
 
--- models/comments.js
+++ models/comments.js
@@ -2,57 +2,83 @@ const db = require('../db')();
 const ObjectID = require('mongodb').ObjectID;
 const COLLECTION = 'issues';
 
-module.exports = () =>{
+module.exports = () => {
 
-    const getCommentsForIssue = async (issueNumber) =>{
-        const PIPELINE =[
-            {$match: {"slug": issueNumber}},
-            {$project:{
-                comments: 1,
-                _id: 0,
-                issueNumber: 1
-            }}
-        ]
-        const getComment = await db.aggregate(COLLECTION , PIPELINE);
-        return getComment;
-    }
-
-        const getOneComment = async (commentId) =>{
+    const getCommentsForIssue = async (issueNumber) => {
+        try {
             const PIPELINE = [
-                {$match: {'comments._id': ObjectID(commentId)}},
-                {$project: {
-                  comments: {$filter: {
-                    input: '$comments',
-                    as: 'comment',
-                    cond: {$eq: ['$$comment._id', ObjectID(commentId)]}
-                  }},
-                  _id: 0,
-                  issueNumber: 1
-                } }
-              ]
-            const comments = await db.aggregate( COLLECTION , PIPELINE);
-            return comments;
-        
+                { $match: { "slug": issueNumber } },
+                {
+                    $project: {
+                        comments: 1,
+                        _id: 0,
+                        issueNumber: 1
+                    }
+                }
+            ]
+            const getComment = await db.aggregate(COLLECTION, PIPELINE);
+            return { getComment };
+        } catch (err) {
+            return {
+                error: err
+            }
         }
-    
-    
-        
-    const addComment = async(issueNumber, text, author) =>{
-        const PIPELINE = [{slug : issueNumber}, {$push:{comments: {
-            _id: new ObjectID(),
-            text: text,
-            author: author,
-
-        }}}]
-        const results = await db.update(COLLECTION, PIPELINE);
-
-        return results.result;
-    
     }
 
-        return {
-            getCommentsForIssue,
-            addComment,
-            getOneComment
+    const getOneComment = async (commentId) => {
+        try {
+            const PIPELINE = [
+                { $match: { 'comments._id': ObjectID(commentId) } },
+                {
+                    $project: {
+                        comments: {
+                            $filter: {
+                                input: '$comments',
+                                as: 'comment',
+                                cond: { $eq: ['$$comment._id', ObjectID(commentId)] }
+                            }
+                        },
+                        _id: 0,
+                        issueNumber: 1
+                    }
+                }
+            ]
+            const comments = await db.aggregate(COLLECTION, PIPELINE);
+            return { comments };
+        } catch (err) {
+            return {
+                error: err
+            }
         }
+    }
+
+
+
+    const addComment = async (issueNumber, text, author) => {
+        try {
+            const PIPELINE = [{ slug: issueNumber }, {
+                $push: {
+                    comments: {
+                        _id: new ObjectID(),
+                        text: text,
+                        author: author,
+
+                    }
+                }
+            }]
+            const results = await db.update(COLLECTION, PIPELINE);
+
+            return results.result;
+        } catch (err) {
+            return {
+                error: err
+            }
+        }
+    }
+
+return {
+    getCommentsForIssue,
+    addComment,
+    getOneComment
+}
     }
